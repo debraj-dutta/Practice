976eb026816a51edd3f82b18552a827ea49f40ec
debraj-dutta <debraj_dutta@outlook.com>
2012-08-31 08:09:08 +0530

    eGIT - Initial

--- src/com/example/practice/MainActivity.java
+++ src/com/example/practice/MainActivity.java
@@ -11,7 +11,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity {
-final String tag = "ticketresponder";
+	final String tag = "ticketresponder";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,7 +20,7 @@ final String tag = "ticketresponder";
 		setContentView(R.layout.activity_main);
 
 		final EditText edittext1 = (EditText) findViewById(R.id.editText1);
-		final Button button1 = (Button )findViewById(R.id.button1);
+		final Button button1 = (Button) findViewById(R.id.button1);
 		final Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
 				this, R.array.spinner1_items,
@@ -27,18 +28,29 @@ final String tag = "ticketresponder";
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner1.setAdapter(adapter);
 
-		button1.setOnClickListener(new Button.OnClickListener(){
-			public void onClick(View v){
+		button1.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
 				String docket = edittext1.getText().toString();
-				String status = spinner1.getSelectedItem().toString();	
-				AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+				String status = spinner1.getSelectedItem().toString();
+				String msg;
+				AlertDialog.Builder adb = new AlertDialog.Builder(
+						MainActivity.this);
 				AlertDialog ad = adb.create();
-				ad.setMessage("Updating docket ["+docket.trim()+"] to [" + status + "]");
+			if (docket.length()== 0 ){
+					msg = "Docket # can't be blanks. Please enter value.";
+			}
+				else
+				{
+					msg = "Updating docket [" + docket.trim() + "] to ["
+							+ status + "].";
+				}
+				ad.setMessage(msg);
 				ad.show();
+				
 			}
 
-		}	);
-		
+		});
+
 	}
 
 	@Override
